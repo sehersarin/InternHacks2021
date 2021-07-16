e805e63805f80e7da13f7dab0741fb89c92cb72c
ashleyyoon19 <ashley.yoon100@gmail.com>
2021-07-17 02:57:46 +0900

    Update Mentee Login Header

--- src/Components/Mentees/MenteeLoginHeader.js
+++ src/Components/Mentees/MenteeLoginHeader.js
@@ -1,17 +1,44 @@
+import React, { useState } from 'react';
 import logo from "../../Images/logo.JPG";
-import Avatar from '@material-ui/core/Avatar';
+import { Menu, MenuItem, Button, Link } from '@material-ui/core';
 import { deepPurple } from '@material-ui/core/colors';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-    purple: {
-        color: theme.palette.getContrastText(deepPurple[500]),
-        backgroundColor: deepPurple[500],
-    },
-}));
+import profilePic from '../../Images/profilePic.png';
+import expandMore from '../../Images/expandMore.png';
+import calendar from '../../Images/calendar.png';
+import chat from '../../Images/chat.png';
+import notification from '../../Images/notification.png';
 
-const MenteeLoginHeader = () => {
-    const classes=useStyles();
+import MenteeProfile from './MenteeProfile';
+import { useAuth } from "../../context/AuthContext";
+import { useHistory } from "react-router-dom";
+import { Alert } from "@material-ui/lab";
+
+function MenteeLoginHeader() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [error, setError] = useState("");
+  const { signout } = useAuth();
+  const history = useHistory();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
+
+  async function handleSignOut() {
+    setError("");
+
+    try {
+      await signout();
+      history.push("/sign-in");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
 
   return (
     <div>
@@ -41,13 +68,55 @@ const MenteeLoginHeader = () => {
               </a>
             </li>
             <li className="nav-item">
-              <a
-                className="nav-link navb btn text-warning rounded"
-                style={{backgroundColor: "#14213D"}}
-                href="/mentee-profile"
-              >
-                <Avatar className={classes.purple}>A</Avatar>
+              <a className="nav-link navb" href="/chat">
+                <img src={chat} style={{height: '21px', width: '18px', marginRight: '7px'}}/>
               </a>
+            </li>
+            <li className="nav-item">
+              <img src={notification} style={{height: '28px', width: '28px', marginBottom: '-13px', marginRight: '2px'}}/>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link navb" href="/schedule">
+                <img src={calendar} style={{height: '28px', width: '28px', marginTop: '-3px', marginRight: '10px'}}/>
+              </a>
+            </li>
+            <li className="nav-item">
+                <img src={profilePic} style={{width: '40px', height: '40px'}}/>
+                <Button 
+                  aria-controls="simple-menu" 
+                  aria-haspopup="true" 
+                  onClick={handleClick}
+                  style={{backgroundColor: 'transparent', height: '20px', marginLeft: '-15px'}}
+                >
+                  <img src={expandMore} style={{width: '20px', height: '20px'}}/>
+                </Button>
+
+                <Menu 
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem 
+                    onClick={handleClose}>
+                      <a href="/mentee-profile" 
+                        style={{color: 'black', textDecorationLine: 'none'}}>
+                        Profile
+                      </a>
+                  </MenuItem>
+                  <MenuItem 
+                    onClick={handleClose}>
+                      <a href="/mentee-edit-settings" 
+                        style={{color: 'black', textDecorationLine: 'none'}}>
+                        Edit Settings
+                      </a>
+                  </MenuItem>
+                  <MenuItem onClick={handleSignOut}>
+                      Sign Out
+                      {/* {error && <Alert severity="error" className="mb-4">{error}</Alert>} */}
+                  </MenuItem>
+                </Menu>
             </li>
           </ul>
         </div>
