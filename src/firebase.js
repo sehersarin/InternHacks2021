60852c4c05ea3ffbe95934a6ffca80ba0d5c54fa
ashleyyoon19 <ashley.yoon100@gmail.com>
2021-07-17 02:58:29 +0900

    Firebase to .env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,22 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyAqH1aVjMJ_Ufpz_WFWsJ9HCiYwn_JqwWc",
-    authDomain: "internhacks-tech-mentor-match.firebaseapp.com",
-    databaseURL: "https://internhacks-tech-mentor-match-default-rtdb.firebaseio.com",
-    projectId: "internhacks-tech-mentor-match",
-    storageBucket: "internhacks-tech-mentor-match.appspot.com",
-    messagingSenderId: "680718584850",
-    appId: "1:680718584850:web:b888f41f03c4c8b6f21ccb",
-    measurementId: "G-S322HHNVPG"
-    // apiKey: process.env.FIREBASE_API_KEY,
-    // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    // databaseURL: process.env.FIREBASE_DATABASE_URL,
-    // projectId: process.env.FIREBASE_PROJECT_ID,
-    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    // appId: process.env.FIREBASE_APP_ID,
-    // measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
 
 export const auth = app.auth()
