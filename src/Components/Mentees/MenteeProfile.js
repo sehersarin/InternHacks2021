d8692701081e628c7872b76b6258875ad1d278d2
ashleyyoon19 <ashley.yoon100@gmail.com>
2021-07-17 02:57:25 +0900

    Update Mentee Profile

--- src/Components/Mentees/MenteeProfile.js
+++ src/Components/Mentees/MenteeProfile.js
@@ -1,46 +1,30 @@
-import { Link, useHistory } from "react-router-dom";
-import { Alert } from "@material-ui/lab";
-import { useAuth } from "../../context/AuthContext";
-import React, { useState } from "react";
-import friends from "../../Images/team.png";
-import { FiSmile } from "react-icons/fi";
-import { GiNothingToSay } from "react-icons/gi";
-import { BsBriefcaseFill } from "react-icons/bs";
+import React from "react";
+import Done from "../../Images/Done.png";
+import portrait from "../../Images/portrait.png";
+import work from "../../Images/work.png";
+import sentiment from "../../Images/sentiment_very_satisfied.png";
+import chat from "../../Images/chat.png";
 
 function MenteeProfile() {
-  const [error, setError] = useState("");
-  const { signout } = useAuth();
-  const history = useHistory();
-
-  async function handleSignOut() {
-    setError("");
-
-    try {
-      await signout();
-      history.push("/sign-in");
-    } catch {
-      setError("Failed to log out");
-    }
-  }
-
   return (
     <div>
       <div className="container">
-        <div className="row m-3 p-3">
+        <div className="row p-3">
           <div className="col-3">
             <img
-              src={friends}
-              class="img-thumbnail h-75 w-75 border border-warning p-4"
+              src={portrait}
+              class="img-thumbnail h-80 w-75 border border-warning"
+              style={{borderRadius: "10px"}}
               alt="..."
             ></img>
           </div>
           <div className="col">
-            <h2>Hi, I am John Doe</h2>
+            <h2>Hi, I'm John Doe</h2>
             <p>Software Engineering Intern</p>
             <h5>Facebook</h5>
           </div>
           <div className="col-3">
-            <a className="navb btn btn-dark text-warning rounded" href="#">
+            <a className="navb btn text-warning pl-3 pr-3" href="#" style={{backgroundColor: "#14213D", borderRadius: "10px"}}>
               <b>Book a Session</b>
             </a>
           </div>
@@ -49,70 +33,85 @@ function MenteeProfile() {
       <hr />
       <div className="row m-2 p-2">
         <div className="col-5">
-          <h5>About me</h5>
-          <p className="h6">
-            I am a junior at Columbia University mojoring in Computer Science. I
-            have previously interned at Apple and now at Facebook
+          <h5 className="pb-2"><b>About me</b></h5>
+          <p style={{fontSize: "18px"}}>
+            I am a junior at Columbia University majoring in Computer Science. I
+            have previously interned at Apple and now at Facebook...
+            <a href="#" style={{color: "#FCA311"}}>
+              See more
+            </a>
           </p>
-          <FiSmile /> I am mentoring on
-          <span className="font-weight-bold">
-            {" "}
-            Carrer Advice, Interview prep
-          </span>
-          <br></br>
-          <GiNothingToSay /> I am fluent in
-          <span className="font-weight-bold"> Java, C , C++</span>
-          <br></br>
-          <BsBriefcaseFill /> Ask me about
-          <span className="font-weight-bold"> SWE, Blockchain Technology</span>
+          <div className="pt-3">
+            <img src={sentiment} style={{marginRight: "10px"}} /> I'm mentoring on
+            <span className="font-weight-bold">
+              {" "}
+              Career Advice, Interview Prep
+            </span>
+          </div>
+          <div className="pt-3 pb-3">
+            <img src={chat} style={{marginLeft: "3px", marginRight: "10px"}}/> I'm fluent in
+            <span className="font-weight-bold"> Java, C, C++</span>
+          </div>
+          <div className="pb-3">
+            <img src={work} style={{marginRight: "10px"}}/> Ask me about
+            <span className="font-weight-bold"> SWE, Blockchain Technology</span>
+          </div>
           <hr />
           <h5>Industry</h5>
           <div className="d-flex">
-            <p className="m-3">
-              <span className="border border-warning rounded p-2">
-                Technology
+            <p className="mt-3 mr-3">
+              <span className="border border-warning shadow pl-4 pr-4 pt-2 pb-2"
+                style={{borderRadius: "10px"}}>
+                <b>Technology</b>
               </span>
             </p>
-            <button className="ml-auto  btn btn-dark text-warning rounded">
-              Edit
+            <button className="ml-auto btn text-warning" style={{backgroundColor: "#14213D", 
+              borderRadius: "10px"}}>
+              <b>Edit</b>
             </button>
           </div>
           <hr />
           <h5>Interests</h5>
           <div className="d-flex">
-            <p className="m-3 ">
-              <span className="border border-warning rounded p-2">
-                Deep Learning
+            <p className="mt-3 mr-3">
+              <span className="border border-warning shadow pl-4 pr-4 pt-2 pb-2"
+                style={{borderRadius: "10px"}}>
+                <b>Deep Learning</b>
               </span>
             </p>
             <p className="m-3">
-              <span className="border border-warning rounded p-2">
-                AI & Ethics
+              <span className="border border-warning shadow pl-4 pr-4 pt-2 pb-2"
+                style={{borderRadius: "10px"}}>
+                <b>AI & Ethics</b>
               </span>
             </p>
             <p className="m-3">
-              <span className="border border-warning rounded p-2">Gaming</span>
+              <span className="border border-warning shadow pl-4 pr-4 pt-2 pb-2" style={{borderRadius: "10px"}}>
+                <b>Gaming</b>
+              </span>
             </p>
-            <button className="ml-auto  btn btn-dark text-warning rounded">
-              Edit
+            <button className="ml-auto btn text-warning" style={{backgroundColor: "#14213D", borderRadius: "10px"}}>
+              <b>Edit</b>
             </button>
           </div>
           <hr />
         </div>
+        <div className="container" style={{width: "80px"}}/>
         <div className="col-1"></div>
         <div className="col">
           <h3>Reviews</h3>
-          <div className="card text-white bg-dark rounded">
-            <div className="card-body ">
-              Tell us about your mentorship experience
+          <div className="card text-white pb-3 pl-2" style={{backgroundColor: "#14213D", borderRadius: "10px"}}>
+            <div className="card-body" style={{backgroundColor: "#14213D", borderRadius: "10px"}}>
+              <b style={{fontSize: "20px"}}>Tell us about your mentorship experience</b>
             </div>
-            <button className="w-50 bg-white text-warning rounded ml-3">
-              Write a review
+            <button className="bg-white text-warning ml-3 pt-3 pb-3" style={{borderRadius: "10px", width: "230px"}}>
+              <b style={{fontSize: "23px"}}>Write a Review</b>
             </button>
           </div>
           <img
-            src={friends}
-            class="img-thumbnail w-100 h-50 border p-4 mt-2"
+            src={Done}
+            class="img-thumbnail p-4 mt-2"
+            style={{width: "70%", borderColor: "transparent", display: "block", marginLeft: "auto", marginRight: "auto"}}
             alt="..."
           ></img>
           <h5 className="d-flex justify-content-center">No reviews yet</h5>
